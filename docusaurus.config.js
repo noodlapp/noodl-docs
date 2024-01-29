ea5bae357a0c9de6184f72afb2bdea252a900596
kotte <konstantin.aistre@gmail.com>
2024-01-29 14:23:24 +0100

    feat: make sure the search dropdown is visible

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,14 +53,9 @@ const config = {
       '@easyops-cn/docusaurus-search-local',
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       {
-        // ... Your options.
-        // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
-        // For Docs using Chinese, The `language` is recommended to set to:
-        // ```
-        // language: ["en", "zh"],
-        // ```
-      }
+        searchBarPosition: 'left',
+      },
     ],
 
     // Node reference docs
